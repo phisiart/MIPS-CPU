00337da856210b96d1292f4d7234b4201c93e466
phisiart <phisiart@gmail.com>
2014-07-04 15:56:50 +0800

    clean up

--- Assembler/src.s
+++ Assembler/src.s
@@ -1,5 +1,5 @@
-        addi $s2 $zero 10         # line 1
-        addi $s0 $zero 1          # line 2
+        li $s2 10         # line 1
+        li $s0 1          # line 2
 loop:   add $s1 $s1 $s0   # line 3
         addi $s0 $s0 1    # line 4
         bne $s0 $s2 loop  # line 5
