190f0e0dd2b41c2cc8a448d19a2dbd6184cc6737
seanyuki <seanyukigeek@gmail.com>
2014-07-09 16:39:45 +0800

    finish Euclidean
    Euclidean Algorithm to find greatest common divisor. Something wrong
    with my Spim simulator's keyboard IO and I can't use syscall to read an
    integer.

---
+++ MIPS/Euclidean.s
@@ -0,0 +1,42 @@
+main:
+	li $a0, 8
+	li $a1, 5
+	jal Euclidean
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	li $v0, 10
+	syscall
+
+Euclidean:
+	# save $s0, $s1 to the stack
+	addi $sp, $sp, -8
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+
+	# load $a0, $a1 into $s0, $s1
+	add $s0, $a0, $zero
+	add $s1, $a1, $zero
+LOOP:
+	slt $t0, $s0, $s1
+	beq $t0, $zero, NOT_SWITCH
+
+	# switch $s0 and $s1 to make sure $s0 > $s1
+	add $t0, $s0, $zero
+	add $s0, $s1, $zero
+	add $s1, $t0, $zero
+
+NOT_SWITCH:
+	sub $t0, $s0, $s1
+
+	# if $t0 == 0, then we have found the greatest common divisor
+	beq $t0, $zero, RETURN
+	add $s0, $t0, $zero
+	j LOOP
+
+RETURN:
+	add $v0, $s0, $zero
+	lw $s1, 4($sp)
+	lw $s0, 0($sp)
+	addi $sp, $sp, 8
+	jr $ra
